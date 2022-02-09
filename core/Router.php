76ceedeed947ad08c9826ec198fa886f9b0a39bd
benjamim <bhaskdelta2@gmail.com>
2022-02-08 22:23:04 -0300

    add: routing

--- core/Router.php
+++ core/Router.php
@@ -2,10 +2,33 @@
 
 namespace app\core;
 
+use JetBrains\PhpStorm\Pure;
+
 class Router
 {
-    public function get($path, $function)
-    {
+    protected array $routes = [];
+    public Request $request;
 
+    public function __construct(Request $request) {
+        $this->request = $request;
+    }
+
+    public function get(string $path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        $path = $this->request->getPaht();
+        $method = $this->request->getMethod();
+        $callback = $this->routes[$method][$path] ?? false;
+
+        if (!$callback) {
+            echo 'Not Found';
+            exit;
+        }
+
+        echo call_user_func($callback);
     }
 }
--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
+use app\core\Application;
+use app\core\Request;
 
-$app = new \app\core\Application();
+$app = new Application();
 
-$router = $app->router;
+$app->router->get('/', fn() => 'Hello World');
+$app->router->get('/contact', fn() => 'Contact');
 
-$router->get('/', function () use ($app) {});
+$app->run();
