2b80411d062c2ef433fed7bdb6a9c1d644b3308b
benjamim <bhaskdelta2@gmail.com>
2022-02-16 22:38:37 -0300

    finish registration

---
+++ views/register.php
@@ -0,0 +1,27 @@
+<h1>Create a new account</h1>
+
+<form action="" method="post">
+    <div class="col"><div class="mb-3">
+        <label  class="form-label">First name</label>
+        <input type="text" name="firstname" class="form-control">
+    </div></div>
+    <div class="col"><div class="mb-3">
+        <label class="form-label">Last name</label>
+        <input type="text" name="lastname" class="form-control">
+    </div></div>
+
+
+    <div class="mb-3">
+        <label class="form-label">Email</label>
+        <input type="text" name="email" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Password</label>
+        <input type="password" name="password" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Confirm Password</label>
+        <input type="password" name="confirmpassword" class="form-control">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
